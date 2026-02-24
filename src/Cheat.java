f640a6d3467ddaba861cfd37a4d5ceab6542e715
Dannik <diepvt@mail.uc.edu>
2026-02-24 07:05:05 -0500

    Added Cheat class.

---
+++ src/Cheat.java
@@ -0,0 +1,25 @@
+
+
+
+public class Cheat {
+
+//    @Override
+    public String getMove(String playerMove) {
+        String computerMove = "";
+        switch (playerMove) {
+            case "R":
+                computerMove = "P";
+                break;
+            case "P":
+                computerMove = "S";
+                break;
+            case "S":
+                computerMove = "R";
+                break;
+            default:
+                computerMove = "X";
+                break;
+        }
+        return computerMove;
+    }
+}
