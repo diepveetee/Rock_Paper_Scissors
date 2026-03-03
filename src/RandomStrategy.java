061a1dfd19ad1dc06ce4a27912fbdc51abf4c207
Dannik <diepvt@mail.uc.edu>
2026-03-02 22:25:17 -0500

    changed formatting of switch case

--- src/RandomStrategy.java
+++ src/RandomStrategy.java
@@ -7,9 +7,15 @@
  */
 
 public class RandomStrategy implements Strategy {
+
+    @Override
     public String getMove(String playerMove) {
         int r = (int)(Math.random() * 3);
-        return (r == 0 ? "R" : r == 1 ? "P" : "S");
+
+        return switch (r) {
+            case 0 -> "R";
+            case 1 -> "P";
+            default -> "S";
+        };
     }
 }
-
